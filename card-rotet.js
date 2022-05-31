2fed857db031a97d94347f2b98d4ccc6ad7f040d
SergeyBelozub <99657458+SergeyBelozub@users.noreply.github.com>
2022-05-31 17:29:13 +0200

    cd

--- card-rotet.js
+++ card-rotet.js
@@ -1,4 +1,17 @@
-$('button#test').click(function (e) {
-	e.preventDefault();
-	$(this).find('front.cart').toggleClass('active');
-});
+function turnRight() {
+    turnEllipse(180);
+  }
+  
+  function turnLeft() {
+    turnEllipse(-180);
+  }
+  
+  function turnEllipse(degrees) {
+    var angle = $("#superellips").data("angle");
+    if (!angle)
+      angle = 0;
+    angle = +angle + degrees;
+    $("#superellips")
+      .data("angle", angle)
+      .css({ transform: "rotate(" + angle + "deg)", transition: "1s" });
+  }
