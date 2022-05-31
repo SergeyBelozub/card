cd02387090e4e45c5efb93ceb454618cff5a30eb
SergeyBelozub <99657458+SergeyBelozub@users.noreply.github.com>
2022-05-31 18:53:49 +0200

    вфы

--- card-rotet.js
+++ card-rotet.js
@@ -1,17 +1,41 @@
-function turnRight() {
-    turnEllipse(180);
-  }
-  
-  function turnLeft() {
-    turnEllipse(-180);
-  }
-  
-  function turnEllipse(degrees) {
-    var angle = $("#superellips").data("angle");
-    if (!angle)
-      angle = 0;
-    angle = +angle + degrees;
-    $("#superellips")
-      .data("angle", angle)
-      .css({ transform: "rotate(" + angle + "deg)", transition: "1s" });
-  }
+import './index.scss';
+
+const select = document.querySelector('.select');
+const button = document.querySelector('.sidebar-menu__button_imd-main')
+
+function close () {
+    select.classList.remove ('open');
+}
+function open () {
+    select.classList.add ('open');
+}
+function toggle () {
+    if (select.classList.contains('open')){
+        close ();
+    }else {
+        open ();
+    }
+}
+
+button.onclick = toggle;
+
+
+const mobileButton = document.querySelector('.sidebar-mobile-burger');
+const sidebarMenu = document.querySelector('.main-content');
+
+function closeSidebar () {
+    sidebarMenu.classList.remove ('main-content-mobile-open');
+}
+function opeSidebarn () {
+    sidebarMenu.classList.add ('main-content-mobile-open');
+}
+function mobileToggle () {
+    if (sidebarMenu.classList.contains('main-content-mobile-open')){
+        closeSidebar ();
+    }else {
+        opeSidebarn ();
+    }
+}
+
+mobileButton.onclick = mobileToggle;
+document.addEventListener('swiped-left', closeSidebar());
